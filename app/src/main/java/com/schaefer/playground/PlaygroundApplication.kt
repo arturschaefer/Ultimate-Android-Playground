c82520fe255b2c05164dada31e204498dcce1583
Artur Schaefer <artur.schaefer@glovoapp.com>
2023-12-12 16:38:09 -0300

    chore: first commit

---
+++ app/src/main/java/com/schaefer/playground/PlaygroundApplication.kt
@@ -0,0 +1,10 @@
+package com.schaefer.playground
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class PlaygroundApplication: Application() {
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.kotlinKsp) apply false
+    alias(libs.plugins.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
